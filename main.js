afa4370f990153225714546592e5bd7b9293c488
tuk09 <152992409+tuk09@users.noreply.github.com>
2023-12-19 09:14:04 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const root = document.querySelector('#root')
+const btnAdd = document.querySelector('.btn-add')
+const sumInfo = document.querySelector('.show-sum')
+
+let total = 0 
+
+function Counter() {
+let countNum = 0
+function makeElement(tag, attr_n, attr_v, content) {
+let output = document.createElement(tag)
+output.setAttribute(attr_n, attr_v)
+output.textContent = content
+return output
+}
+
+const updateCounter = (n) => {
+if(countNum + n < 0) {
+return
+}
+countNum += n
+total += n
+number.textContent = countNum
+sumInfo.textContent = `Sum = ${total}`
+}
+
+const delCounter = (e) => {
+updateCounter(-countNum)
+e.target.closest('.counter').remove()
+}
+
+const counter = makeElement('div', 'class', 'counter','')
+const btnInc = makeElement('button', 'class', 'btn-inc','+')
+const number = makeElement('h3', 'class', 'number','0')
+const btnDec = makeElement('button', 'class', 'btn-dec','-')
+const btnClr = makeElement('button', 'class', 'btn-clr','0')
+const btnDel = makeElement('button', 'class', 'btn-del','x')
+
+btnInc.addEventListener('click', () => updateCounter(1))
+btnDec.addEventListener('click', () => updateCounter(-1))
+btnClr.addEventListener('click', () => updateCounter(-countNum))
+btnDel.addEventListener('click' , delCounter)
+
+// counter.append(btnInc, number, btnDec, btnClr, btnDel)
+ counter.append(btnInc)
+ counter.append(number)
+ counter.append(btnDec)
+ counter.append(btnClr)
+ counter.append(btnDel)
+
+return counter
+}
+
+const hdlAddCounter = () => {
+root.append(Counter())
+}
+
+btnAdd.addEventListener('click', hdlAddCounter)
